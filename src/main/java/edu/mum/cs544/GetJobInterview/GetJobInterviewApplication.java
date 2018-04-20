0fd5935b41826531c1d72b68c0d63dc2e6cb4470
edward-pc <eddytnk@gmail.com>
2018-04-20 11:32:56 -0500

    Added project structure

---
+++ src/main/java/edu/mum/cs544/GetJobInterview/GetJobInterviewApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.cs544.GetJobInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetJobInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetJobInterviewApplication.class, args);
+	}
+}
