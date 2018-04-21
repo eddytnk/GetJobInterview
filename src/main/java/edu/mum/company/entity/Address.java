8f0ece0a311efe3c70ea63f8dbc6d044a11978e1
chauky <32781912+chauky@users.noreply.github.com>
2018-04-20 20:33:50 -0500

    Add properties for Entities in Company

---
+++ src/main/java/edu/mum/company/entity/Address.java
@@ -0,0 +1,46 @@
+package edu.mum.company.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	private String street;
+	private String state;
+	private String country;
+	private int zipcode;
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public int getZipcode() {
+		return zipcode;
+	}
+	
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
+	
+}
--- src/main/java/edu/mum/company/entity/Company.java
+++ src/main/java/edu/mum/company/entity/Company.java
@@ -1,5 +1,92 @@
 package edu.mum.company.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Company {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long Id;
+	private String name;
+	private String emailAddress;
+	private String website;
+	
+	@OneToMany
+	@JoinColumn(name="company_id")
+	private List<Category> categories = new ArrayList<Category>();
+	
+	@Embedded
+	private Address address;
+
+	public Long getId() {
+		return Id;
+	}
+	
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	
+	public String getWebsite() {
+		return website;
+	}
+
+	
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	
+	
+	public void addCategory(Category cat) {
+		categories.add(cat);
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 }
